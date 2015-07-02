8ca58b1a04e618eef1f5809bd25d00b851256f54
Dustin Specker <dustinspecker@dustinspecker.com>
2015-07-02 15:32:48 -0500

    chore(test.js) fix newline typos

--- test.js
+++ test.js
@@ -5,17 +5,17 @@ var newlineRegex = require('./')
   , expect = require('chai').expect;
 
 describe('newline-regex', function () {
-  it('should match Unix new line', function () {
+  it('should match Unix newline', function () {
     /*jshint -W030 */
     expect(newlineRegex.test('Unix\n')).to.be.true;
   });
 
-  it('should match Windows new line', function () {
+  it('should match Windows newline', function () {
     /*jshint -W030 */
     expect(newlineRegex.test('Windows\r\n')).to.be.true;
   });
 
-  it('should not match when no new line', function () {
+  it('should not match when no newline', function () {
     /*jshint -W030 */
     expect(newlineRegex.test('Nothing')).to.be.false;
   });
