866a944a840ae20e73de1fd5dc67822c3cc0f1f0
Dustin Specker <dustinspecker@dustinspecker.com>
2015-07-03 22:46:15 -0500

    refactor(all) convert to ES2015

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,6 @@
 'use strict';
+import babel from 'gulp-babel';
+import babelCompiler from 'babel';
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import istanbul from 'gulp-istanbul';
@@ -7,8 +9,10 @@ import jshint from 'gulp-jshint';
 import mocha from 'gulp-mocha';
 
 const configFiles = './gulpfile.babel.js'
-  , srcFiles = 'index.js'
-  , testFiles = 'test.js';
+  , srcFiles = 'src/*.js'
+  , testFiles = 'test.js'
+
+  , destDir = './lib';
 
 gulp.task('lint', () => {
   return gulp.src([configFiles, srcFiles, testFiles])
@@ -23,7 +27,16 @@ gulp.task('lint', () => {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('build', ['lint']);
+gulp.task('compile', ['lint'], () => {
+  return gulp.src(srcFiles)
+    .pipe(babel({
+      auxiliaryCommentBefore: 'istanbul ignore next',
+      modules: 'common'
+    }))
+    .pipe(gulp.dest(destDir));
+});
+
+gulp.task('build', ['compile']);
 
 gulp.task('test', ['build'], (cb) => {
   gulp.src(['index.js'])
@@ -31,7 +44,11 @@ gulp.task('test', ['build'], (cb) => {
     .pipe(istanbul.hookRequire())
     .on('finish', () => {
       gulp.src([testFiles])
-        .pipe(mocha())
+        .pipe(mocha({
+          compilers: {
+            js: babelCompiler
+          }
+        }))
         .pipe(istanbul.writeReports())
         .on('end', cb);
     });
--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 /* global describe, it */
 /* eslint no-unused-expressions:0 */
 'use strict';
-var newlineRegex = require('./')
-  , expect = require('chai').expect;
+import {expect} from 'chai';
+import newlineRegex from './lib/';
 
-describe('newline-regex', function () {
-  describe('default export', function () {
-    it('should match Unix newline', function () {
+describe('newline-regex', () => {
+  describe('default export', () => {
+    it('should match Unix newline', () => {
       /*jshint -W030 */
       expect(newlineRegex.test('Unix\n')).to.be.true;
     });
 
-    it('should match Windows newline', function () {
+    it('should match Windows newline', () => {
       /*jshint -W030 */
       expect(newlineRegex.test('Windows\r\n')).to.be.true;
     });
 
-    it('should not match when no newline', function () {
+    it('should not match when no newline', () => {
       /*jshint -W030 */
       expect(newlineRegex.test('Nothing')).to.be.false;
     });
   });
 
-  describe('g export', function () {
-    it('should find 2 newline characters', function () {
+  describe('g export', () => {
+    it('should find 2 newline characters', () => {
       /*jshint -W030 */
       expect('Unix\n and Windows\r\n'.match(newlineRegex.g).length).to.eql(2);
     });
